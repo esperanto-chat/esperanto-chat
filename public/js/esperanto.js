a4b9a056faf76b0643ea9449b3cf1ce11cd55375
Moshe Stauber <Moshe.stauber89@gmail.com>
2016-06-19 18:47:13 -0400

    Updated UI

--- public/js/esperanto.js
+++ public/js/esperanto.js
@@ -1,3 +1,18 @@
 /**
  * Created by itc_user on 6/19/2016.
  */
+//getAllRooms(user)
+//getMsgsForRoom(roomID)
+//postMsg(text,roomID)
+
+$(document).ready(function() {
+	// alert($('.conversation').first().attr('class'));
+
+    $('.conversation').click(function(e) { 
+    	$('.active-chat').removeClass('active-chat');
+     	$(this).addClass('active-chat');
+    });
+
+	$('.conversation').first().click();
+
+});
