d5b0c66c6ce3e6eead41e2a40c80e4264592fb91
shlomo <shlomoshlomoshlomo@gmail.com>
2016-06-19 18:48:29 -0400

    update backend methods

--- routes/getmessagesforroom.js
+++ routes/getmessagesforroom.js
@@ -1,19 +1,18 @@
 var getmessagesforroom = function(id, lang){
   var results = db.messages.find({
     room_id: parseInt(id)
-  });
+  }).sort({timestamp: 1});
   messages = [];
   for (i = 0; i < results.length(); i++){
     result = results[i];
     text = result.translations[lang];
     sender = result.sender;
+    timestamp = result.timestamp;
     messages[i] = {
       "text": text,
-      "sender" : sender
+      "sender" : sender,
+      "timestamp" : timestamp
     }
-    // messages["text"] = text;
-    // messages["sender"] = sender;
-
   }
   return messages;
 }
--- routes/postmsg.js
+++ routes/postmsg.js
@@ -1,13 +1,24 @@
-var postmsg = function(text, room_id, user){
+var postmsg = function(text, room, user, lang){
+  if (lang == null){
+    lang = "en";
+  }
   var message = {
     sender: user,
-    source_lang: src_lng,
-    translations: {
-    },
-    room_id: room_id,
+    source_lang: lang,
+    translations: {},
+    room_id: room,
     timestamp: new Date()
   };
-
-  message.translations[src_]
+  message.translations[lang] = text;
   db.messages.insert(message);
 }
+
+// lang = "en";
+// var message = {
+//   sender: "shmag18",
+//   source_lang: lang,
+//   translations[lang] : "text",
+//   room_id: 1,
+//   timestamp: new Date()
+// };
+// db.messages.insert(message);
