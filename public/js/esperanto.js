18547dac5e185eb554ee58b323658b9fa1f3e715
Moshe Stauber <Moshe.stauber89@gmail.com>
2016-06-20 02:19:48 -0400

    UI update

--- public/js/esperanto.js
+++ public/js/esperanto.js
@@ -6,10 +6,36 @@
 //postMsg(text,roomID)
 
 $(document).ready(function() {
-	// alert($('.conversation').first().attr('class'));
 
-    $('.conversation').click(function(e) {
+	//getAllRooms(user)  //AJAX request
+
+    $('.conversation').click(function(e) { 
     	$('.active-chat').removeClass('active-chat');
      	$(this).addClass('active-chat');
+
+     	var list = $('#message-list');
+     	list.empty();
+
+     	// getmessagesforroom(1,'es').each(function(val){
+     	// 	list.append("<li class='media'>" +
+      //             "<div class='media-body'>" +
+      //               "<div class='media'>" +
+      //                 "<div class='media-body'>" +
+      //                   val.text +
+      //                   "<br />" +
+      //                   "<small class='text-muted'>" +
+      //                   val.sender + "|" + val.timestamp + "</small>" +
+      //                   "<hr />" +
+      //                 "</div>" +
+      //               "</div>" +
+      //             "</div>" + 
+      //           "</li>" +
+      //           );
+     	// });
+
+
     });
+
+	$('.conversation').first().click();
+
 });
